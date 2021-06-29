9d8b16e8014f7728956fbef900a059f669601864
AfraaAlobaid <afraa.obaid1@gmail.com>
2021-06-29 12:26:07 +0930

    handle props

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -1,18 +1,25 @@
-import React, { Component } from 'react';
-import MovieCard from './card-components/MovieCard.js'
-import movieData from './data.js'
+import React, { Component } from "react";
+import MovieCard from "./card-components/MovieCard.js";
+import movieData from "./data.js";
 
 export default class MovieShowcase extends Component {
-
   generateMovieCards = () => {
     // map over your movieData array and return an array of the correct JSX
-  }
+    return movieData.map((movieObj) => (
+      <MovieCard
+        title={movieObj.title}
+        IMDBRating={movieObj.IMDBRating}
+        genres={movieObj.genres}
+        poster={movieObj.poster}
+      />
+    ));
+  };
 
   render() {
     return (
       <div id="movie-showcase">
         {this.generateMovieCards()}
       </div>
-    )
+    );
   }
 }
